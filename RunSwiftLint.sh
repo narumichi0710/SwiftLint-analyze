c1bfb83d39bba7f0e79713afee3650075e6d4ddc
narumichikubo <narumichi7373@gmail.com>
2022-08-08 22:57:55 +0900

    add script for Lint

---
+++ RunSwiftLint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# SwiftLintの実行スクリプト
+export PATH="$PATH:/opt/homebrew/bin"
+
+if which swiftlint > /dev/null; then
+  swiftlint
+else
+  echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+fi
