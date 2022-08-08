5c2f74667b515e546e6226d92ce5528afbba6b8e
narumichikubo <narumichi7373@gmail.com>
2022-08-08 22:18:38 +0900

    init

---
+++ SwiftLint-analyze/SwiftLint_analyzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLint_analyzeApp.swift
+//  SwiftLint-analyze
+//
+//  Created by Narumichi Kubo on 2022/08/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLint_analyzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
