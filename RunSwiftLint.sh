345c3cee5a2d5539f829f12fd2dc1c38bf988507
narumichikubo <narumichi7373@gmail.com>
2022-08-09 10:42:29 +0900

    changed projectName

--- RunSwiftLint.sh
+++ RunSwiftLint.sh
@@ -8,3 +8,14 @@ if which swiftlint > /dev/null; then
 else
   echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
 fi
+
+# Analyzeの実行スクリプト
+project="SwiftLint-analyze.xcodeproj"
+scheme="SwiftLint-analyze"
+
+xcodebuild clean -project ${project} -scheme ${scheme}
+
+xcodebuild -project ${project} -scheme ${scheme} -sdk iphonesimulator -configuration Debug -destination "platform=iOS Simulator,name=iPhone 13" > xcodebuild.log
+
+swiftlint analyze --autocorrect --compiler-log-path xcodebuild.log --fix
+
