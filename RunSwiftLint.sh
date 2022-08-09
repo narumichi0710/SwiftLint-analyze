0781e2c3a5c40aa9dd504b4e410688ab3ceea4ae
narumichikubo <narumichi7373@gmail.com>
2022-08-09 11:58:57 +0900

    fix command

--- RunSwiftLint.sh
+++ RunSwiftLint.sh
@@ -15,7 +15,7 @@ scheme="SwiftLintAnalyze"
 
 xcodebuild clean -project ${project} -scheme ${scheme}
 
-xcodebuild -project ${project} -scheme ${scheme} -sdk iphonesimulator -configuration Debug -destination "platform=iOS Simulator,name=iPhone 13,OS=15" > xcodebuild.log
+xcodebuild -project ${project} -scheme ${scheme} -sdk iphonesimulator -configuration Debug -destination "platform=iOS Simulator,name=iPhone 13,OS=15.0" > xcodebuild.log
 
 swiftlint analyze --autocorrect --compiler-log-path xcodebuild.log
 
